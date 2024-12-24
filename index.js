5973427712dad2aadd59f705237b3128778f22e6
imwrdo <sheinnickolas@gmail.com>
2024-12-24 16:30:39 +0100

    feat: made imports

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
-
 app.use(cors())
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
+
+
+
+app.use(express.static('public'))
+app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post('/api/users', function (req, res) {
+  const username = req.body.username;
+  
+});
 
 
 
